044be189c6a3c1d2253f15523904d2843a0a6880
kavyarathod05 <rathodkavya2005@gmail.com>
2025-05-14 14:12:47 +0530

    removed forgot password

--- client/clientt/MERN-ecommerce-Frontend-077f0daacdba186f21367b5068bec791951cc4f5/src/features/auth/components/Login.js
+++ client/clientt/MERN-ecommerce-Frontend-077f0daacdba186f21367b5068bec791951cc4f5/src/features/auth/components/Login.js
@@ -75,14 +75,14 @@ export default function Login() {
                 >
                   Password
                 </label>
-                <div className="text-sm">
+                {/* <div className="text-sm">
                   <Link
                     to="/forgot-password"
                     className="font-semibold text-indigo-600 hover:text-indigo-500"
                   >
                     Forgot password?
                   </Link>
-                </div>
+                </div> */}
               </div>
               <div className="mt-2">
                 <input
