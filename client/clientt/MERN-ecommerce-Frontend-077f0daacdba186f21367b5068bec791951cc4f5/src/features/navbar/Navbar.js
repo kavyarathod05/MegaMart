50ea7b4ca9af6900de216af1c61587469c18e7ea
kavyarathod05 <rathodkavya2005@gmail.com>
2025-05-14 13:13:50 +0530

    forgot password changes in frontend

--- client/clientt/MERN-ecommerce-Frontend-077f0daacdba186f21367b5068bec791951cc4f5/src/features/navbar/Navbar.js
+++ client/clientt/MERN-ecommerce-Frontend-077f0daacdba186f21367b5068bec791951cc4f5/src/features/navbar/Navbar.js
@@ -11,8 +11,8 @@ import { selectItems } from "../cart/cartSlice";
 import { selectLoggedInUser } from "../auth/authSlice";
 
 const navigation = [
-  { name: "Dashboard", link: "#", user: true },
-  { name: "Team", link: "#", user: true },
+  // { name: "Dashboard", link: "#", user: true },
+  // { name: "Team", link: "#", user: true },
   { name: "Admin", link: "/admin", admin: true },
   { name: "Orders", link: "/admin/orders", admin: true },
 ];
